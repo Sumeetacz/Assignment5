bad31c432519b2a3a84a7a4ce59de2e3fcf75b9e
Sumeetacz <sumeetacz@gmail.com>
2022-03-14 23:11:34 +0530

    [assignment] day5 UC1 to 10

---
+++ Factor_UC5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicCoreProgram
+{
+    internal class Factor
+    {
+
+        public void Factors()
+        {
+            Console.Write("Enter the Numbe: ");
+            int Num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} Prime Factors is = ", Num);
+
+            for (int i = 2; i * i <= Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ HarmonicNumber_UC4/HarmonicNumber_UC4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class HarmonicNumber
+    {
+
+        static float sum(float n)
+        {
+            // Base condition
+            if (n < 2)
+                return 1;
+
+            else
+                return 1 / n + (sum(n - 1));
+        }
+
+        // Driven Code
+        public static void Main()
+        {
+            Console.WriteLine(sum(8));
+            Console.WriteLine(sum(10));
+        }
+    }
+}
